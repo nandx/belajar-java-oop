bdf364b95bb2d4436c46e4d046478de5853913c2
nanda <nandaceria@gmail.com>
2022-07-24 18:59:52 +0700

    Initial commit

---
+++ src/main/java/com/belajar/java/BelajarJavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaOopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belajar/java/BelajarJavaOopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
